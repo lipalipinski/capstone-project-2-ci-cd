06aa70b7d0feba65f8d9d9132ed50631231ab5a7
lipalipinski <lipabass@gmail.com>
2023-08-22 23:32:56 +0200

    tf backend

---
+++ tf-provision/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket            = "tf-backend-jlipinski-1"
+    key               = "tf/petclinic-app/terraform.tfstate"
+    region            = "eu-central-1"
+    dynamodb_endpoint = "dynamodb.eu-central-1.amazonaws.com"
+    dynamodb_table    = "tf-lock-jlipinski-1"
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+
+  default_tags {
+    tags = {
+      Name    = "jlipinski-petclinic"
+      Owner   = "jlipinski"
+      Project = "2023_internship_waw"
+    }
+  }
+}
