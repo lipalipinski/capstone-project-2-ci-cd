74f69426c83e1a65f838e9081d60a7da2cf5aa1a
lipalipinski <lipabass@gmail.com>
2023-08-24 10:58:49 +0200

    app server sg from everywhere

--- tf-provision/security-groups.tf
+++ tf-provision/security-groups.tf
@@ -8,7 +8,8 @@ resource "aws_security_group" "app_server_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.app_lb_sg.id]
+    # security_groups = [aws_security_group.app_lb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
