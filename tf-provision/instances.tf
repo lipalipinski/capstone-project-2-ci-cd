ca5d8376fc47c48ccac0fac9f66bd13050831cbc
lipalipinski <lipabass@gmail.com>
2023-08-24 10:28:50 +0200

    install apache2 on app server

--- tf-provision/instances.tf
+++ tf-provision/instances.tf
@@ -9,6 +9,11 @@ module "app-server-1" {
   monitoring    = true
   subnet_id     = data.aws_subnets.private.ids[0]
 
+  user_data = <<EOF
+  apt-get update -y && apt-get upgrade -y
+  apt-get install -y apache2
+EOF
+
   tags = {
     Name  = "app_server_1"
     Group = "app_server"
