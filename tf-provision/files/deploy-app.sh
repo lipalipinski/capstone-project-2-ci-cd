5431e0256156031351133bbf3f16f5d7c830bfd0
lipalipinski <lipabass@gmail.com>
2023-08-29 13:51:21 +0200

    deploy

--- tf-provision/files/deploy-app.sh
+++ tf-provision/files/deploy-app.sh
@@ -3,4 +3,4 @@
 # stop any containers
 docker stop $(docker ps -aq)
 
-docker run -d --name apache2-container -e TZ=UTC -p 80:80 ubuntu/apache2:2.4-22.04_beta
+docker run -d -e TZ=UTC -p 80:80 ubuntu/apache2:2.4-22.04_beta
