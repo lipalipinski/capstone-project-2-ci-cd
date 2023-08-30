02d51d0f757e1f4edcfbdd0c9c93421334e821f9
lipalipinski <lipabass@gmail.com>
2023-08-30 09:02:20 +0200

    deploy petclinic:tag

--- tf-provision/files/deploy-app.sh
+++ tf-provision/files/deploy-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+APP_TAG="$1"
+
 # stop any containers
 docker stop $(docker ps -aq)
 
-docker run -d -e TZ=UTC -p 80:80 ubuntu/apache2:2.4-22.04_beta
+docker run -d -e TZ=UTC -p 80:80 jlipinski-petclinic:$APP_TAG
