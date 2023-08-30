af5089590f40143592034d01189ed50b4a80faac
lipalipinski <lipabass@gmail.com>
2023-08-30 09:08:08 +0200

    login to ecr in deploy script

--- tf-provision/files/deploy-app.sh
+++ tf-provision/files/deploy-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 APP_TAG="$1"
+ECR_REGISTRY_URL="$2"
+
+aws ecr get-login-password --region eu-central-1 \
+  | docker login --username AWS --password-stdin ECR_REGISTRY_URL
 
 # stop any containers
 docker stop $(docker ps -aq)
