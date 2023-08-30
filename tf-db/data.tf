b0068361ce2defdf192ddcc3163acd94dd9390a5
lipalipinski <lipabass@gmail.com>
2023-08-30 14:41:09 +0200

    tf-db, UP tf db stages

---
+++ tf-db/data.tf
@@ -0,0 +1,28 @@
+
+data "aws_vpcs" "app-vpc" {
+  tags = {
+    Name = "petclinic-jlipinski"
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpcs.app-vpc.ids[0]]
+  }
+
+  tags = {
+    Tier = "private"
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpcs.app-vpc.ids[0]]
+  }
+
+  tags = {
+    Tier = "public"
+  }
+}
---
+++ tf-db/variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  type    = string
+  default = ""
+}
+
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "gd-boundry-policy" {
+  # set to meet GD AWS policy
+  type    = string
+  default = "arn:aws:iam::113304117666:policy/DefaultBoundaryPolicy"
+}
