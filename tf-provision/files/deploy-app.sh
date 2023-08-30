66a7a1b99c21fa8d3710fe9d54d7bb8264338eb9
lipalipinski <lipabass@gmail.com>
2023-08-30 10:21:32 +0200

    deploy script echo messages

--- tf-provision/files/deploy-app.sh
+++ tf-provision/files/deploy-app.sh
@@ -3,11 +3,17 @@
 APP_TAG="$1"
 ECR_REGISTRY_URL="$2"
 
+echo "Loging to ECR..."
 aws ecr get-login-password --region eu-central-1 \
   | docker login --username AWS --password-stdin $ECR_REGISTRY_URL
 
 # stop any containers
+echo "Stopping any running containers..."
 docker stop $(docker ps -aq)
 
+# pull app image
+echo "Pulling $ECR_REGISTRY_URL:$APP_TAG\..."
 docker pull -q $ECR_REGISTRY_URL:$APP_TAG
+
+echo "Running $ECR_REGISTRY_URL:$APP_TAG\..."
 docker run -d -p 80:8080 $ECR_REGISTRY_URL:$APP_TAG
