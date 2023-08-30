5447343b4bcb5260ff8213c6b14a55a993817ae0
lipalipinski <lipabass@gmail.com>
2023-08-30 10:28:14 +0200

    Deploy job print alb address at the end

--- tf-provision/files/deploy-app.sh
+++ tf-provision/files/deploy-app.sh
@@ -3,17 +3,18 @@
 APP_TAG="$1"
 ECR_REGISTRY_URL="$2"
 
-echo "Loging to ECR..."
+echo -e "\nLoging to ECR..."
 aws ecr get-login-password --region eu-central-1 \
   | docker login --username AWS --password-stdin $ECR_REGISTRY_URL
 
 # stop any containers
-echo "Stopping any running containers..."
+echo -e "\nStopping any running containers..."
 docker stop $(docker ps -aq)
 
 # pull app image
-echo "Pulling $ECR_REGISTRY_URL:$APP_TAG\..."
+echo -e "\nPulling $ECR_REGISTRY_URL:$APP_TAG\..."
 docker pull -q $ECR_REGISTRY_URL:$APP_TAG
 
-echo "Running $ECR_REGISTRY_URL:$APP_TAG\..."
+
+echo -e "\nRunning $ECR_REGISTRY_URL:$APP_TAG\..."
 docker run -d -p 80:8080 $ECR_REGISTRY_URL:$APP_TAG
