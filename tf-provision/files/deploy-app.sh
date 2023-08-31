3bb24af650157d318db3badf978bb696c4fc056e
lipalipinski <lipabass@gmail.com>
2023-09-01 00:34:03 +0200

    deploy db endpoint

--- tf-provision/files/deploy-app.sh
+++ tf-provision/files/deploy-app.sh
@@ -29,7 +29,7 @@ echo -e "\nRunning $ECR_REGISTRY_URL:$APP_TAG\..."
 docker run -d \
   -p 80:8080 \
   -e SPRING_PROFILES_ACTIVE=mysql \
-  -e MYSQL_URL="jdbc:mysql://jlipinski-petclinic-db.cv7gyy9x6999.eu-central-1.rds.amazonaws.com/petclinic:3306" \
+  -e MYSQL_URL="jdbc:mysql://jlipinski-petclinic-db.cv7gyy9x6999.eu-central-1.rds.amazonaws.com/petclinic:3306/" \
   -e MYSQL_USER=admin \
   -e MYSQL_PASS="$DB_PASS" \
   "$ECR_REGISTRY_URL:$APP_TAG"
